ee3816f2fc62cba69399669999339472c60c170c
Paul D'Amora <paul.damora@rover.com>
2019-09-11 16:14:37 -0700

    Suppress hydration warnings

--- src/hook.tsx
+++ src/hook.tsx
@@ -91,6 +91,7 @@ export default function useLazyHydration(
     <div
       ref={childRef}
       style={{ display: "contents" }}
+      suppressHydrationWarning
       dangerouslySetInnerHTML={{ __html: "" }}
     />
   );
